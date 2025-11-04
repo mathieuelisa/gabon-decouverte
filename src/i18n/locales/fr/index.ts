b85cae2bbc86b068b1a3f75cf263e2e7fd9b890b
bibo <bibo@corp.free.fr>
2025-11-04 12:05:05 +0100

    delete old file

--- src/i18n/locales/fr/index.ts
+++ src/i18n/locales/fr/index.ts
@@ -1,11 +1,8 @@
 import common from './common.json'
-import features from './pages'
 
 const fr = {
-	common,
-	...features
+	common
 }
 
-export * from './pages'
 export type TLocalFr = typeof fr
 export default fr
