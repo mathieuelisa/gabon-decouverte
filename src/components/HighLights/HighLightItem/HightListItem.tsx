4eeceb8ba2d757656d04c5ff08e858034f4348fe
Mills <mathieu.elisa@gmail.com>
2025-11-07 22:02:17 +0100

    feat: update highlights

--- src/components/HighLights/HighLightItem/HightListItem.tsx
+++ src/components/HighLights/HighLightItem/HightListItem.tsx
@@ -1,8 +1,8 @@
 export default function HightListItem({ description, title }) {
 	return (
 		<section className='flex max-w-80 flex-col sup-md:text-left text-center'>
-			<div className='mb-2.5 text-greeny-100 text-xl'>{title}</div>
-			<div className='font-display text-md'>{description}</div>
+			<h2 className='mb-2.5 text-greeny-100 text-xl'>{title}</h2>
+			<p className='font-display text-md'>{description}</p>
 		</section>
 	)
 }
