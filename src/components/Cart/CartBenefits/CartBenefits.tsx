6f05a88ce59aaa8c39b1ffbfd053eb3cc2c6442f
Mills <mathieu.elisa@gmail.com>
2025-11-16 12:17:49 +0100

    feat; update sur le composant cart benefits

--- src/components/Cart/CartBenefits/CartBenefits.tsx
+++ src/components/Cart/CartBenefits/CartBenefits.tsx
@@ -6,7 +6,7 @@ import { CART_BENEFITS_DATA } from './CartBenefitsMock.data'
 
 export default function CartBenefits() {
 	return (
-		<div className='flex w-72 flex-col gap-3 rounded-lg border border-gray-200 p-3'>
+		<div className='flex flex-col gap-3 rounded-lg border border-gray-200 p-3'>
 			{CART_BENEFITS_DATA.map((item) => (
 				<section className='flex items-center gap-3' key={item.id}>
 					<GrValidate className='h-5 w-5 text-greeny-100' />
