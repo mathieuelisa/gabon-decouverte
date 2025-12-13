26f6b232e78f49df1d18e5f3e3470104d7c214b4
Mills <mathieu.elisa@gmail.com>
2025-12-13 23:27:46 +0100

    feat: update

--- src/components/Cart/CartItemCard/CartItemCard.tsx
+++ src/components/Cart/CartItemCard/CartItemCard.tsx
@@ -23,7 +23,9 @@ export default function CartItemCard({ item, onRemove }: TcartItemCard) {
 			className='flex min-w-[60%] sup-md:flex-row flex-col gap-2 rounded-md border border-gray-100 p-3'
 			key={item.id}
 		>
-			{item.img ? <Image alt='img' className='w-full' height={300} src={item.img} width={300} /> : null}
+			{item.img ? (
+				<Image alt='img' className='w-full rounded-sm' height={300} src={item.img} width={300} />
+			) : null}
 
 			<div className='flex w-full flex-col justify-between'>
 				<section className='flex justify-between'>
